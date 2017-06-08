540caf00ad7a4495f6e8ab1758964ee402be9d8c
GimYoungPhil <youngphil.gim@gmail.com>
2017-06-08 15:38:29 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # desk2017
+
+```sh
+$ node -v
+v6.10.3
+
+$ npm -v
+5.0.1
+
+$ yarn --version
+0.24.6
+```
---
+++ app/app.js
@@ -0,0 +1,10 @@
+let test = "renderIndentGuides";
+let arr = ["1", "2", "3"];
+
+let fn = () => {
+  console.log();
+
+  for(let i; i > arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
